11471daf399b4e32826a52db0b74249a627c88e0
Bluephoenix <biendien4508@gmail.com>
2024-05-16 16:18:55 +0700

    config env

--- models/index.js
+++ models/index.js
@@ -5,10 +5,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
+require('dotenv').config()
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
